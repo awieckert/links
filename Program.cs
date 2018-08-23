553bf485102ab1e9f069d93cbcd3d4bd87abee06
Adam Wieckert <wiec1369@gmail.com>
2018-08-23 15:17:46 -0500

    Figured out how to Reverse the names and order by last name

--- Program.cs
+++ Program.cs
@@ -8,6 +8,12 @@ using System.Collections.Generic;
     public IEnumerable<string> Millionaires { get; set; }
 
   }
+  public class Bank
+{
+    public string Symbol { get; set; }
+    public string Name { get; set; }
+}
+// Define a customer
 public class Customer
 {
     public string Name { get; set; }
@@ -139,7 +145,58 @@ public class Program
           Console.WriteLine($"{item.Bank}: {item.Millionaires.Count()}");
       }
 
+        // Create some banks and store in a List
+        List<Bank> banks = new List<Bank>() {
+            new Bank(){ Name="First Tennessee", Symbol="FTB"},
+            new Bank(){ Name="Wells Fargo", Symbol="WF"},
+            new Bank(){ Name="Bank of America", Symbol="BOA"},
+            new Bank(){ Name="Citibank", Symbol="CITI"},
+        };
 
+        // Create some customers and store in a List
+        List<Customer> customers7 = new List<Customer>() {
+            new Customer(){ Name="Bob Lesman", Balance=80345.66, Bank="FTB"},
+            new Customer(){ Name="Joe Landy", Balance=9284756.21, Bank="WF"},
+            new Customer(){ Name="Meg Ford", Balance=487233.01, Bank="BOA"},
+            new Customer(){ Name="Peg Vale", Balance=7001449.92, Bank="BOA"},
+            new Customer(){ Name="Mike Johnson", Balance=790872.12, Bank="WF"},
+            new Customer(){ Name="Les Paul", Balance=8374892.54, Bank="WF"},
+            new Customer(){ Name="Sid Crosby", Balance=957436.39, Bank="FTB"},
+            new Customer(){ Name="Sarah Ng", Balance=56562389.85, Bank="FTB"},
+            new Customer(){ Name="Tina Fey", Balance=1000000.00, Bank="CITI"},
+            new Customer(){ Name="Sid Brown", Balance=49582.68, Bank="CITI"}
+        };
+
+        /*
+    TASK:
+    As in the previous exercise, you're going to output the millionaires,
+    but you will also display the full name of the bank. You also need
+    to sort the millionaires' names, ascending by their LAST name.
+
+    Example output:
+        Tina Fey at Citibank
+        Joe Landy at Wells Fargo
+        Sarah Ng at First Tennessee
+        Les Paul at Wells Fargo
+        Peg Vale at Bank of America
+*/
+
+        List<Customer> millionaireReport = customers7.Where(n => n.Balance >= 1000000).Select(c => new Customer(){
+          Name = c.Name,
+          Bank = banks.Find(b => c.Bank == b.Symbol).Name
+        }).ToList();
+
+        foreach (Customer customer in millionaireReport)
+        {
+          customer.Name = String.Join(" ", customer.Name.Split(' ').Reverse());
+        }
+
+        List<Customer> orderedMillionaires = millionaireReport.OrderBy(n => n.Name).ToList();
+
+        foreach (Customer customer in orderedMillionaires)
+        {
+            Console.WriteLine($"{customer.Name} at {customer.Bank}");
+        }
 
 
 
