6355327bbf9a95fcc9897516e80423dd54d1d53b
Adam Wieckert <wiec1369@gmail.com>
2018-08-23 14:29:23 -0500

    First groupings project

--- Program.cs
+++ Program.cs
@@ -2,6 +2,18 @@
 using System.Linq;
 using System.Collections.Generic;
 
+  public class GroupedMillionaires {
+    public string Bank { get; set; }
+
+    public IEnumerable<string> Millionaires { get; set; }
+
+  }
+public class Customer
+{
+    public string Name { get; set; }
+    public double Balance { get; set; }
+    public string Bank { get; set; }
+}
 public class Program
 {
     public static void Main() {
@@ -88,6 +100,49 @@ public class Program
 
       IEnumerable<int> theNonSquares = wheresSquaredo.TakeWhile(n => Math.Sqrt(n) % 1 != 0);
       Console.WriteLine($"All the not squares {String.Join(", ", theNonSquares)}");
+
+      /*
+          Given the same customer set, display how many millionaires per bank.
+          Ref: https://stackoverflow.com/questions/7325278/group-by-in-linq
+
+          Example Output:
+          WF 2
+          BOA 1
+          FTB 1
+          CITI 1
+      */
+      List<Customer> customers = new List<Customer>() {
+        new Customer(){ Name="Bob Lesman", Balance=80345.66, Bank="FTB"},
+        new Customer(){ Name="Joe Landy", Balance=9284756.21, Bank="WF"},
+        new Customer(){ Name="Meg Ford", Balance=487233.01, Bank="BOA"},
+        new Customer(){ Name="Peg Vale", Balance=7001449.92, Bank="BOA"},
+        new Customer(){ Name="Mike Johnson", Balance=790872.12, Bank="WF"},
+        new Customer(){ Name="Les Paul", Balance=8374892.54, Bank="WF"},
+        new Customer(){ Name="Sid Crosby", Balance=957436.39, Bank="FTB"},
+        new Customer(){ Name="Sarah Ng", Balance=56562389.85, Bank="FTB"},
+        new Customer(){ Name="Tina Fey", Balance=1000000.00, Bank="CITI"},
+        new Customer(){ Name="Sid Brown", Balance=49582.68, Bank="CITI"}
+      };
+
+      var groupedByBank = customers.Where(c => c.Balance >= 1000000).GroupBy(
+        p => p.Bank,
+        p => p.Name,
+        (bank, millionaires) => new GroupedMillionaires()
+        {
+          Bank = bank,
+          Millionaires = millionaires
+        }
+      ).ToList();
+
+      foreach (GroupedMillionaires item in groupedByBank)
+      {
+          Console.WriteLine($"{item.Bank}: {item.Millionaires.Count()}");
+      }
+
+
+
+
+
       // Main ends here
     }
 }
